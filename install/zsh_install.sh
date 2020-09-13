3c810908159fc116ae250bee6f07d61582f38bd3
Alexander Mueller <blub@blub.com>
2020-09-13 13:55:33 +0200

    * remove swap files * add install shell script for ZSH * replace loacal paths with general solution * add aliases for editing zshrc and ohmyzsh

---
+++ install/zsh_install.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+# install zsh
+apt install zsh -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
