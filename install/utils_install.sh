81c91a316262ea43e4fe4e36c3df184457bb50a5
Alexander Mueller <blub@blub.com>
2020-09-15 11:53:11 +0200

    add ctags

--- install/utils_install.sh
+++ install/utils_install.sh
@@ -2,5 +2,9 @@
 sudo apt install silversearcher-ag -y
 sudo apt install bat -y
 sudo apt install fd-find -y
+
+# the --all argument will install all without confirmation
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
+
+sudo apt install exuberant-ctags
