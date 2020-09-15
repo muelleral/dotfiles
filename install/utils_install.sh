d4a1366e734c8cf97f39f3a78f359624b22fc796
Alexander Mueller <blub@blub.com>
2020-09-15 14:09:04 +0200

    add creation of symlinks for bat and fd

--- install/utils_install.sh
+++ install/utils_install.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
-sudo apt install silversearcher-ag -y
+
+mkdir -p ~/.local/bin
+
+# install bat and create a symlink because the installed binary is named batcat due to a name clash
 sudo apt install bat -y
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# install fd and create a symlink because the installed binary is named fdfind due to a name clash
 sudo apt install fd-find -y
+ln -s /usr/bin/fdfind ~/.local/bin/fd
 
 # the --all argument will install all without confirmation
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-sudo apt install exuberant-ctags
-
+# install tmux and create a symlink to the config
 sudo apt install tmux -y
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
+# install a faster grep alternative
+sudo apt install silversearcher-ag -y
+
+sudo apt install exuberant-ctags
