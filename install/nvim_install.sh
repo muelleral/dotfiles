3b2ad3bb3b6a7926d7ccfafa448950b12c5bb5d7
Alexander <blub@blub.com>
2021-01-07 21:35:28 +0100

    -replace leader <space> with <,>; -move CoC plugin config into separate file

--- install/nvim_install.sh
+++ install/nvim_install.sh
@@ -7,22 +7,19 @@ cd -
 chmod u+x ~/.local/bin/nvim.appimage
 ln -s ~/.local/bin/nvim.appimage ~/.local/bin/nvim
 
-sudo apt install nodejs -y
-sudo apt install npm -y
-
-sudo npm install -g neovim
-
 pip3 install neovim
 
+# create a symLink to the nvim configuration
+nvim --headless +"call mkdir(stdpath('config'), 'p')" +"exe 'edit '.stdpath('config').'/init.vim'" +qa
+ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/nvim/coc.vim ~/.config/nvim/coc.vim
+
+# create default local nvim config if none exists
 LOCAL_VIMRC=~/.config/nvim/local.vim
 if [ ! -f $LOCAL_VIMRC ]; then 
     PYTHON_PATH=$(which python3)
     echo "let g:python3_host_prog = '$PYTHON_PATH'" > $LOCAL_VIMRC
 fi
-
-# create a symLink to the nvim configuration
-nvim --headless +"call mkdir(stdpath('config'), 'p')" +"exe 'edit '.stdpath('config').'/init.vim'" +qa
-ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
 # get plugin manager and install plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -30,5 +27,13 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 nvim --headless +PlugInstall +qa
 
 # Configure CoC
-#nvim --headless "+CocInstall coc-python" +qa
+sudo apt install nodejs -y
+sudo apt install npm -y
+
+# nodejs >= 10 required. ubuntu 18.04 provides only 8. 
+# use "curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -" to get required version
+sudo npm install -g neovim
+
+#install coc extensions 
+nvim --headless "+CocInstall coc-python" +qa
 
