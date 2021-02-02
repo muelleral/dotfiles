b754dfc30eff21a450641cba9b60b9f59e7a685b
Alexander <blub@blub.com>
2021-02-02 16:29:38 +0100

    install: get and use path of top level install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # update sources
-sudo apt update
-sudo apt upgrade -y
+sudo apt update && apt upgrade -y
+
+# get path to this file
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo $SCRIPTPATH
 
 # call isntall scripts
-sh install/zsh_install.sh
-sh install/python_install.sh
-sh install/utils_install.sh
-sh install/nvim_install.sh
+sh $SCRIPTPATH/install/zsh_install.sh
+sh $SCRIPTPATH/install/python_install.sh
+sh $SCRIPTPATH/install/utils_install.sh
+sh $SCRIPTPATH/install/nvim_install.sh
