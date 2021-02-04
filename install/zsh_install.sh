7277ef544fccb4845fb8aac83f190125851b846d
Alexander <blub@blub.com>
2021-02-04 20:43:19 +0100

    zsh: move oh-my-zsh into XDG_CONFIG_HOME/oh-my/zsh

--- install/zsh_install.sh
+++ install/zsh_install.sh
@@ -4,6 +4,8 @@
 sudo apt install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+ZSH_CUSTOM=$HOME/.config/oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -17,9 +19,13 @@ if [ -f $ZSHRC ]; then
 fi
 
 if [ ! -f $ZSHRC ]; then 
-    echo 'export ZSH="$HOME/.oh-my-zsh"' > $ZSHRC
+    echo 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then' > $ZSHRC
+    echo '  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"' >>$ZSHRC
+    echo 'fi' >> $ZSHRC
+
+    echo 'export ZSH="${XDG_CONFIG_HOME:-$HOME/.config}/oh-my-zsh"' >> $ZSHRC
     echo 'export ZSH_DOT_DIR="$HOME/dotfiles/zsh"' >> $ZSHRC
-    echo 'source $HOME/dotfiles/zsh/zshrc' >> $ZSHRC
+    echo 'source $ZSH_DOT_DIR/zshrc' >> $ZSHRC
 fi
 
 
