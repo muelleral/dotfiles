506bcb7281c2d5f5517ecd42449eced0fe45bd7d
Alexander <blub@blub.com>
2021-02-06 18:41:03 +0100

    install zsh: install ohmyzsh in .config/ohmyzsh

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 # update sources
 sudo apt update && apt upgrade -y
 
+ZSHRC=~/.zshrc
+if [ -f $ZSHRC ]; then 
+    mv $ZSHRC $ZSHRC.old
+
+    # the '-' allows the heredoc to be indented 
+    # the ' surrounding the delimeter leads to not expanding the $VARs in the multiline comment
+	cat <<- 'EOF' > $ZSHRC
+	[[ -z "$XDG_CONFIG_HOME" ]] && export XDG_CONFIG_HOME="$HOME/.config"
+	[[ -z "$XDG_DATA_HOME" ]] && export XDG_DATA_HOME="$HOME/.local/share"
+	EOF
+fi
+
 # get path to this file
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPTPATH
