889c66d362a25906b3463489c289dda7b1f5856f
Alexander Mueller <blub@blub.com>
2021-02-07 11:48:49 +0100

    install:     - refactor top level install script     - add XDG settings and PATH extension to bashrc and zshrc if       available

--- install.sh
+++ install.sh
@@ -1,28 +1,69 @@
 #!/bin/sh
-# update sources
-sudo apt update && apt upgrade -y
+# Default settings
+SKIP_ZSHRC=${SKIP_ZSHRC:-no}     # add XDG settings and PATH to zshrc. Skip with --skip-zsh
+SKIP_BASHRC=${SKIP_BASHRC:-no}   # add XDG settings and PATH to bashrc. Skip with --skip-bash
 
-ZSHRC=~/.zshrc
-if [ -f $ZSHRC ]; then 
-    mv $ZSHRC $ZSHRC.old
-fi
+# Other settings
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # get path to this file
+RC_FILE=$HOME/.bashrc
 
-if [ ! -f $ZSHRC ]; then 
+
+appendXDGandPATHToRcFile() {
     # the '-' allows the heredoc to be indented 
     # the ' surrounding the delimeter leads to not expanding the $VARs in the multiline comment
-	cat <<- 'EOF' > $ZSHRC
+	cat <<- 'EOF' >> $RC_FILE
+
 	[[ -z "$XDG_CONFIG_HOME" ]] && export XDG_CONFIG_HOME="$HOME/.config"
 	[[ -z "$XDG_DATA_HOME" ]] && export XDG_DATA_HOME="$HOME/.local/share"
 	export PATH=$HOME/.local/bin:/usr/local/bin:bin$PATH
 	EOF
-fi
+}
 
-# get path to this file
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo $SCRIPTPATH
+
+installEnvironment() {
+# update sources
+sudo apt update && apt upgrade -y
 
 # call isntall scripts
 sh $SCRIPTPATH/install/zsh_install.sh
 sh $SCRIPTPATH/install/python_install.sh
 sh $SCRIPTPATH/install/utils_install.sh
 sh $SCRIPTPATH/install/nvim_install.sh
+}
+
+
+main() {
+    # Parse arguments
+    while [ $# -gt 0 ]; do
+        case $1 in
+            --skip-zsh) ZSHRC=yes ;;
+            --skip-bash) BASHRC=yes ;;
+        esac
+        shift
+    done
+
+    if [ $SKIP_BASHRC = no ]; then
+        RC_FILE=$HOME/.bashrc
+        if [ ! -f $RC_FILE ]; then 
+            echo "$RC_FILE not found. Creating new!"
+            touch $RC_FILE
+        fi
+        appendXDGandPATHToRcFile
+    fi
+
+    if [ $SKIP_ZSHRC = no ]; then
+        RC_FILE=$HOME/.zshrc
+        if [ -f $RC_FILE ]; then 
+            mv $RC_FILE $RC_FILE.old
+            echo "$RC_FILE found. Create backup $RC_FILE.old!"
+        else
+            echo "$RC_FILE not found. Creating new!"
+            touch $RC_FILE
+        fi
+        appendXDGandPATHToRcFile
+    fi
+
+    installEnvironment
+}
+
+main "$@"
