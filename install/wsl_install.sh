8287c528be4afa6b4c59c9b00a14efaabb6d658e
Alexander Mueller <alexander_mueller@freenet.de>
2021-03-23 14:21:59 +0100

    install: add wsl_install.sh script for wsl specific installations

---
+++ install/wsl_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+mv /tmp/win32yank.exe ~/.local/bin
+
