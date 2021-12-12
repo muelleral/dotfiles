198dac956d26bbd74cfb1878fe711233c52690c5
Alexander Mueller <alexander_mueller@freenet.de>
2021-12-12 21:56:01 +0100

    nvim: use plugin to manage maintenance of language servers
    * use nvim-lsp-install plugin
    * use auto install functionality
    * move lsp config into separate file

--- install/nvim_lsp.sh
+++ install/nvim_lsp.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-# ------------
-# lsp
-# ------------
-sudo npm i -g pyright
-sudo npm i -g bash-language-server
-sudo npm i -g dockerfile-language-server-nodejs
-sudo npm i -g vscode-langservers-extracted
-
-sudo pip3 install cmake-language-server
-
-sudo apt-get install clangd-12 -y
-sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
-
-sudo yarn global add yaml-language-server
 
 # ------------
 # linters
--- nvim/config/plugin/nvim-cmp.lua
+++ nvim/config/plugin/nvim-cmp.lua
@@ -1,8 +1,3 @@
--- lsp
-vim.api.nvim_exec([[call minpac#add('neovim/nvim-lspconfig')]], false)
-vim.api.nvim_exec([[call minpac#add('folke/trouble.nvim')]], false)
-require("trouble").setup{}
-
 -- completion engine
 vim.api.nvim_exec([[call minpac#add('hrsh7th/nvim-cmp')]], false)
 vim.api.nvim_exec([[call minpac#add('onsails/lspkind-nvim')]], false)
@@ -97,58 +92,9 @@ cmp.setup({
         { name = 'path' },
         { name = 'luasnip' },
         { name = 'nvim_lsp'},
-        { name = 'buffer', keyword_length = 5 }
+        { name = 'buffer', keyword_length = 5 } -- completion starts only after 5th char
     }
 })
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    local opts = { noremap=true, silent=true }
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-end
-
-local nvim_lsp = require('lspconfig')
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-local servers = { 'bashls',
-'clangd',
-'cmake',
-'dockerls',
-'jsonls',
-'pyright',
-'yamlls'}
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        capabilities = capabilities,
-    }
-end
 
 -- ================================
 -- luasnip config
