ff1d5e6b48802325e88841d5bc9f08570b13441f
alexander mueller <alexander_mueller@freenet.de>
2022-06-29 13:03:59 +0200

    nvim: restrict treesitter to only used file types

--- nvim/config/plugin/treesitter.lua
+++ nvim/config/plugin/treesitter.lua
@@ -1,11 +1,24 @@
 vim.api.nvim_exec([[call minpac#add('nvim-treesitter/nvim-treesitter', {'do': 'TSUpdate'})]], false)
 
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = { "javascript" }, -- List of parsers to ignore installing
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "dockerfile",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "norg",
+        "python",
+        "toml",
+        "vim",
+        "yaml"
+    }, -- "all",one of "all" or a list of languages
     highlight = {
         enable = true,              -- false will disable the whole extension
-        disable = {"rust" },  -- list of language that will be disabled
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
