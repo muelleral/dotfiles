eeddeb858487cd18fcce552ce729b399305b53e4
alexander mueller <alexander_mueller@freenet.de>
2022-07-08 15:41:43 +0200

    nvim: setup telescope via packer

---
+++ nvim/lua/config/telescope.lua
@@ -0,0 +1,19 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+telescope.load_extension('fzf')
+telescope.load_extension('neoclip')
+
+vim.keymap.set("n", "<leader>ff", '<Cmd>Telescope find_files<CR>', {silent = true})
+vim.keymap.set("n", "<leader>fa", '<Cmd>Telescope find_files hidden=true no_ignore=true<CR>', {silent = true})
+vim.keymap.set("n", "<leader>fg", '<Cmd>Telescope grep_string<CR>', {silent = true})
+vim.keymap.set("n", "<leader>flg", '<Cmd>Telescope live_grep<CR>', {silent = true})
+vim.keymap.set("n", "<leader>fb", '<Cmd>Telescope buffers<CR>', {silent = true})
+vim.keymap.set("n", "<leader>fs", '<Cmd>Telescope spell_suggest<CR>', {silent = true})
+vim.keymap.set("n", "<leader>ft", '<Cmd>Telescope<CR>', {silent = true})
+
+-- call neoclip with plus argument to copy selection to plus register which
+-- is the shared register on WSL https://github.com/AckslD/nvim-neoclip.lua#usage
+vim.keymap.set("n", "<space>p", '<Cmd>Telescope neoclip<CR>', {silent = true})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,32 @@
 return require("packer").startup({function(use)
   use 'wbthomason/packer.nvim'
+  use "lewis6991/impatient.nvim" -- speeds up nvim start
   -- Your plugins here
-  use "lewis6991/impatient.nvim"
+  -- Search
+  use {
+		{
+			'nvim-telescope/telescope.nvim',
+			requires = {
+				'nvim-lua/plenary.nvim',
+				'nvim-lua/popup.nvim',
+				'telescope-fzf-native.nvim'
+			},
+			config =[[require('config.telescope')]]
+		},
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			run = 'make'
+		},
+		{
+			'AckslD/nvim-neoclip.lua',
+			config = function()
+				-- yank to plus register to see yank history. 
+				-- Is required cause i use plus as nvim default register for yank/put
+				require("neoclip").setup({default_register={'+'}})
+			end
+		}
+}
+
 end,
 config = {
   display = {
