8008377f32b8b43fa9af53b0632275f2fef1ac99
alexander mueller <alexander_mueller@freenet.de>
2022-07-29 16:29:25 +0200

    nvim: repalce nvim-lint and formatter iwth null-ls

--- nvim/lua/config/nvim-lint.lua
+++
@@ -1,6 +0,0 @@
-require('lint').linters_by_ft = {
-    lua = {'luacheck',},
-    python = {'pylint',}
-}
-
-vim.api.nvim_exec([[au InsertLeave,BufWritePost <buffer> lua require('lint').try_lint()]], false)
