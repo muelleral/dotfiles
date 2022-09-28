16e359078f79acae883b14188ae51bf0833bc231
alexander mueller <alexander_mueller@freenet.de>
2022-09-28 07:42:51 +0200

    refactor: enable partial nvim setup installation and update

--- install/nvim_install.sh
+++ install/nvim_install.sh
@@ -1,43 +1,83 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" # get path to this file
 
-# install nvim as appimage (in sugguested location ${HOME}/Applications/ or ${HOME}/.local/bin/ or ${HOME}/bin/)
-cd ~/.local/bin 
-curl -LO https://github.com/neovim/neovim/releases/download/v0.7.2/nvim.appimage
-chmod u+x nvim.appimage
+# Default settings
+INSTALL_NVIM=${INSTALL_NVIM:-NO}
+INSTALL_DAP=${INSTALL_DAP:-NO}
+INSTALL_ALL=${INSTALL_ALL:-YES}
 
-# extract image to use it the same way in systems with and without fuse installed
-./nvim.appimage --appimage-extract
-ln -s squashfs-root/usr/bin/nvim nvim
-cd -
-
-pip3 install neovim
-sudo npm install -g neovim
-
-# dap setup
-cd ~/.local/bin 
-curl -LO https://github.com/microsoft/vscode-cpptools/releases/download/v1.12.4/cpptools-linux.vsix
-unzip cpptools-linux.vsix -d cpptools
-chmod u+x cpptools/extension/debugAdapters/bin/OpenDebugAD7
-cd -
-
-# configure nvim 
-ln -s $SCRIPTPATH/../nvim/init.lua ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/init.lua
-# create symlinks for files instead of complete lua dir. This allows to create additional configs 
-# which are not port of this repo (e.g. private.lua)
-ln -s $SCRIPTPATH/../nvim/lua/config ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config
-ln -s $SCRIPTPATH/../nvim/lua/commands.lua ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/commands.lua
-ln -s $SCRIPTPATH/../nvim/lua/keymap.lua ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/keymap.lua
-ln -s $SCRIPTPATH/../nvim/lua/options.lua /${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/options.lua
-ln -s $SCRIPTPATH/../nvim/lua/plugins.lua /${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/plugins.lua
-
-# get plugin manager and install plugins
-sh -c 'git clone --depth 1 https://github.com/wbthomason/packer.nvim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim'
-~/.local/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 
+NVIM_VERSION=v0.7.2
+# NVIM_VERSION=nightly
 CPPTOOLS_VERSION=1.12.4
+
+installNvim() {
+  # install nvim as appimage (in sugguested location ${HOME}/Applications/ or ${HOME}/.local/bin/ or ${HOME}/bin/)
+  cd ~/.local/bin 
+  curl -LO https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim.appimage
+  chmod u+x nvim.appimage
+
+  # extract image to use it the same way in systems with and without fuse installed
+  ./nvim.appimage --appimage-extract
+  cd -
+}
+
+installDAP() {
   cd ~/.local/bin 
   curl -LO https://github.com/microsoft/vscode-cpptools/releases/download/v$CPPTOOLS_VERSION/cpptools-linux.vsix
   unzip cpptools-linux.vsix -d cpptools
   chmod u+x cpptools/extension/debugAdapters/bin/OpenDebugAD7
   rm cpptools-linux.vsix
   cd -
+}
+
+bootstrapNvim() {
+  ln -s squashfs-root/usr/bin/nvim nvim
+  pip3 install neovim
+  sudo npm install -g neovim
+
+  # configure nvim 
+  ln -s $SCRIPTPATH/../nvim/init.lua ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/init.lua
+  # create symlinks for files instead of complete lua dir. This allows to create additional configs 
+  # which are not port of this repo (e.g. private.lua)
+  ln -s $SCRIPTPATH/../nvim/lua/config ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config
+  ln -s $SCRIPTPATH/../nvim/lua/commands.lua ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/commands.lua
+  ln -s $SCRIPTPATH/../nvim/lua/keymap.lua ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/keymap.lua
+  ln -s $SCRIPTPATH/../nvim/lua/options.lua /${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/options.lua
+  ln -s $SCRIPTPATH/../nvim/lua/plugins.lua /${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/plugins.lua
+
+  # get plugin manager and install plugins
+  sh -c 'git clone --depth 1 https://github.com/wbthomason/packer.nvim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim'
+  ~/.local/bin/nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 
+}
+
+main() {
+  # Parse arguments
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      --nvim)
+        INSTALL_NVIM=YES
+        INSTALL_ALL=NO
+        shift # past argument
+        ;;
+      --dap)
+        INSTALL_DAP=YES
+        INSTALL_ALL=NO
+        shift # past argument
+        ;;
+    esac
+  done
+
+
+  if [ $INSTALL_ALL = YES ]; then
+    installNvim
+    installDAP
+    bootstrapNvim
+  elif [[ $INSTALL_NVIM = YES ]]; then
+    installNvim
+  elif [[ $INSTALL_DAP = YES ]]; then
+    installDAP
+  fi
+
+}
+
+main "$@"
