38360aa3e4edbac51de9ea4bffac3fcdfbfa6a93
Alexander Mueller <alexander_mueller@freenet.de>
2022-09-28 23:33:44 +0200

    nvim: replace deprecated lsp-installer with mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ require('options')
 require('keymap')
 require('commands')
 require('plugins')
+require('mason').setup() -- make sure mason setup is called before lspconfig
 require('config.lsp')
 
 local private_config_available, private_config = pcall(require, "private")
