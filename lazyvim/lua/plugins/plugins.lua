e0dee70c1e7dad0c2729b157c853c28008e43bba
Alexander Mueller <alexander_mueller@freenet.de>
2024-08-01 08:28:30 +0200

    refactor(lazyvim): adjust which-key settings to deprications

--- lazyvim/lua/plugins/plugins.lua
+++ lazyvim/lua/plugins/plugins.lua
@@ -38,48 +38,6 @@ return {
     },
   },
   {
-    "folke/which-key.nvim",
-    opts = function()
-      require("which-key").register({
-        mode = { "n", "v" },
-        ["<leader>r"] = { name = "+replace" },
-        ["<leader>a"] = { name = "+search all" },
-      })
-    end,
-  },
-  {
-    "nvim-pack/nvim-spectre",
-    keys = {
-      {
-        "<leader>rr",
-        '<cmd>lua require("spectre").open()<CR>',
-        desc = "Search/Replace",
-      },
-      {
-        "<leader>rw",
-        '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
-        desc = "Search current word",
-      },
-      {
-        "<leader>rw",
-        '<esc><cmd>lua require("spectre").open_visual()<CR>',
-        desc = "Search visual selection",
-        mode = "v",
-      },
-      {
-        "<leader>rW",
-        '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
-        desc = "Search current word on current file",
-      },
-      {
-        "<leader>rW",
-        '<cmd>lua require("spectre").open_file_search()<CR>',
-        desc = "Search visual selection in file",
-        mode = "v",
-      },
-    },
-  },
-  {
     "L3MON4D3/LuaSnip",
     config = function()
       require("luasnip.loaders.from_vscode").load({
@@ -117,6 +75,7 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       -- add a keymap to browse plugin files
+      { "<leader>a", "", desc = "+search all", mode = { "n", "v" } },
       {
         "<leader>af",
         function()
@@ -137,15 +96,6 @@ return {
           require("telescope.builtin").grep_string({ additional_args = { "--hidden", "--no-ignore" } })
         end,
         desc = "String Grep all Files",
-      },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      defaults = {
-        ["<leader>o"] = { name = "+obsidian" },
       },
     },
   },
@@ -195,6 +145,7 @@ return {
     },
     keys = {
       -- add a keymap to browse plugin files
+      { "<leader>o", "", desc = "+obsidian", mode = { "n", "v" } },
       {
         "<leader>on",
         "<cmd>ObsidianNew<cr>",
