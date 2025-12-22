bf4ac0bf1acefde05e1ad1338fc9ee503d3ad871
Alexander Mueller <alexander_mueller@freenet.de>
2025-12-22 10:47:09 +0100

    refactor(neovim): remove plugin `ChatGPT.nvim`

--- lazyvim/lua/plugins/plugins.lua
+++ lazyvim/lua/plugins/plugins.lua
@@ -7,18 +7,6 @@ return {
     },
   },
   {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
