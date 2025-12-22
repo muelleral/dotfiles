1fde52d039a7d324e28ff103857bd8fd42365eff
Alexander Mueller <alexander_mueller@freenet.de>
2025-12-22 12:29:47 +0100

    refactor(neovim): switch `epwalsh/obsidian.nvim` -> `obsidian-nvim/obsidian.nvim`

--- lazyvim/lua/plugins/plugins.lua
+++ lazyvim/lua/plugins/plugins.lua
@@ -61,43 +61,31 @@ return {
     },
   },
   {
-    "nvimdev/dashboard-nvim",
-    optional = true,
+    "folke/snacks.nvim",
     opts = function(_, opts)
-      local projects = {
-        action = "ObsidianQuickSwitch",
-        desc = " Obsidian-Notes",
+      table.insert(opts.dashboard.preset.keys, 3, {
         icon = "󱓩 ",
         key = "o",
-      }
-
-      projects.desc = projects.desc .. string.rep(" ", 43 - #projects.desc)
-      projects.key_format = "  %s"
-
-      table.insert(opts.config.center, 3, projects)
+        desc = "Obsidian Notes",
+        action = ":ObsidianQuickSwitch",
+      })
     end,
   },
   {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
     lazy = false,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
     opts = {
       ui = { enable = false },
       notes_subdir = "0_inbox",
+      picker = {
+        name = "snacks.pick",
+      },
       note_id_func = function(title)
-        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-        -- In this case a note with the title 'My new note' will be given an ID that looks
-        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
         local suffix = ""
         if title ~= nil then
-          -- If title is given, transform it into valid file name.
           suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
         else
-          -- If title is nil, just add 4 random uppercase letters to the suffix.
           for _ = 1, 4 do
             suffix = suffix .. string.char(math.random(65, 90))
           end
@@ -106,42 +94,41 @@ return {
       end,
     },
     keys = {
-      -- add a keymap to browse plugin files
       { "<leader>o", "", desc = "+obsidian", mode = { "n", "v" } },
       {
         "<leader>on",
-        "<cmd>ObsidianNew<cr>",
+        "<cmd>Obsidian new<cr>",
         desc = "New Obsidian File",
       },
       {
         "<leader>fo",
-        "<cmd>ObsidianQuickSwitch<cr>",
-        desc = "ObsidianQuickSwitch",
+        "<cmd>Obsidian open<cr>",
+        desc = "Obsidian Open",
       },
       {
         "<leader>of",
-        "<cmd>ObsidianQuickSwitch<cr>",
-        desc = "ObsidianQuickSwitch",
+        "<cmd>Obsidian open<cr>",
+        desc = "Obsidian Open",
       },
       {
         "<leader>os",
-        "<cmd>ObsidianSearch<cr>",
-        desc = "ObsidianSearch",
+        "<cmd>Obsidian search<cr>",
+        desc = "Obsidian Search",
       },
       {
         "<leader>ot",
-        "<cmd>ObsidianTemplate<cr>",
-        desc = "ObsidianTemplate",
+        "<cmd>Obsidian template<cr>",
+        desc = "Obsidian Template",
       },
       {
         "<leader>od",
-        "<cmd>ObsidianToday<cr>",
-        desc = "ObsidianToday",
+        "<cmd>Obsidian today<cr>",
+        desc = "Obsidian Today",
       },
       {
         "<leader>oy",
-        "<cmd>ObsidianYesterday<cr>",
-        desc = "ObsidianYesterday",
+        "<cmd>Obsidian yesterday<cr>",
+        desc = "Obsidian Yesterday",
       },
     },
   },
