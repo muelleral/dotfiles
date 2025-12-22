875986275ed51e346b6f8a45551e4f7491fbb06b
Alexander Mueller <alexander_mueller@freenet.de>
2025-12-22 10:47:47 +0100

    refactor(neovim): switch `williamboman/mason.nvim` -> `mason-org/mason.nvim`

--- lazyvim/lua/plugins/plugins.lua
+++ lazyvim/lua/plugins/plugins.lua
@@ -146,7 +146,7 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     optional = true,
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
