45c81df0532dc1935ee6610c41572be082b7b649
Alexander Mueller <alexander_mueller@freenet.de>
2025-12-22 15:25:25 +0100

    refactor(wezterm): update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,23 +3,28 @@ local act = wezterm.action
 local mod = "CTRL|SHIFT"
 local config = {}
 
-config.color_scheme = "nordfox"
+config.color_scheme = "Catppuccin Frappe"
 config.scrollback_lines = 10000
 config.font_size = 10
 config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
 config.show_tab_index_in_tab_bar = false
 
-config.default_prog = { "wsl", "-d", "Ubuntu-22.04", "--cd", "~" }
+config.default_domain = "WSL:Ubuntu-22.04"
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
+	{ mods = "LEADER", key = "1", action = act.ActivateTab(0) },
+	{ mods = "LEADER", key = "2", action = act.ActivateTab(1) },
 	{ mods = "LEADER", key = "k", action = act.ActivatePaneDirection("Up") },
 	{ mods = "LEADER", key = "j", action = act.ActivatePaneDirection("Down") },
 	{ mods = "LEADER", key = "l", action = act.ActivatePaneDirection("Right") },
 	{ mods = "LEADER", key = "h", action = act.ActivatePaneDirection("Left") },
 	{ mods = "LEADER", key = "c", action = act.SpawnTab("CurrentPaneDomain") },
+	{ mods = "LEADER", key = "j", action = act.ActivateLastTab },
+	{ mods = "LEADER", key = "x", action = act.CloseCurrentTab({ confirm = false }) },
 	{ mods = "LEADER", key = "|", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ mods = "LEADER", key = "-", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ mods = "LEADER", key = "z", action = act.TogglePaneZoomState },
